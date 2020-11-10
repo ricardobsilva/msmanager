f2a87e4d56907b2ff821cf3dac4a97aa9b27b753
Ricardo <ricardo.b.silva13@gmail.com>
2020-11-10 08:48:13 -0300

    Add login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'customers#index'
+  devise_for :users
   resources :customers do
     post "validate_email", to: 'customers#validate_email', on: :collection
     post "validate_cnpj_cpf", to: 'customers#validate_cnpj_cpf', on: :collection
